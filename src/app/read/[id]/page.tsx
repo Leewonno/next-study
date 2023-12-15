1b1c7b80d9480544fe234253e7536a7963cd74ee
Leewonno <dldnjssh123@naver.com>
2023-12-16 04:18:04 +0900

    [Update] 13강 글 읽기

---
+++ src/app/read/[id]/page.tsx
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export default async function Read(props:any){
+
+    const res = await fetch(`http://localhost:9999/topics/${props.params.id}`)
+    const topic = await res.json();
+
+
+    return(
+        <>
+            <h2>Read</h2>
+            {topic.body}
+        </>
+    )
+}
